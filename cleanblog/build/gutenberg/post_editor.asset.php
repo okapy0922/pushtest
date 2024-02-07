a0a23d141c1ae9eff4df3fe1a1a4ad911b188097
okapy0922 <okapy0922@gmail.com>
2024-02-07 10:47:56 +0900

    first commit

---
+++ cleanblog/build/gutenberg/post_editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '17a18802f8c76907751b');
